9a0079b7b6e45cc3f076067307d7feb0aad36a09
Y AM <yam532004gmail.com>
2023-12-27 19:13:36 +0700

    Am push nav, header, footer

---
+++ resources/views/templates/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Header</title>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&family=Volkhov:ital,wght@0,400;0,700;1,700&family=Yesteryear&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="/app/root/CSS/navbar.css">
+
+  <link rel="stylesheet" href="/app/root/CSS/footer.css">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+  <link href="https://cdn.tailwindcss.com/2.2.19/tailwind.min.css" rel="stylesheet">
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body>
+  
---
+++ resources/views/templates/navbar.php
@@ -0,0 +1,20 @@
+<?php include_once 'header.php' ?>
+<nav class="navbar">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <img src="/app/root/images/logo.png" alt="logo">
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Trang chủ</a></li>
+      <li><a href="#">Đặt lịch</a></li>
+      <li><a href="#">Liên hệ</a></li>
+      <li><a href="#">Về chúng tôi</a></li>
+
+    </ul>
+    <div class="but">
+      <button type="submit" name="login" id="but1">Đăng nhập</button>
+      <button type="submit" name="Signup" id="but2">Đăng ký</button>
+    </div>
+  </div>
+</nav>
+<?php include_once 'footer.php' ?>
